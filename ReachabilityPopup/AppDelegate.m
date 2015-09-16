7c21a71de188d070fc361397e6b7dfdbee75bef0
Savana <savanak@wtechspaces.in>
2015-09-16 13:38:02 +0530

    1. Added bottom pop-up 2. Added .gitignore file

--- ReachabilityPopup/AppDelegate.m
+++ ReachabilityPopup/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Reachability.h"
 
 @interface AppDelegate ()
 
@@ -14,9 +15,14 @@
 
 @implementation AppDelegate
 
-
+Reachability *reachable;
+UIView *noNetView;
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reachabilityChanged:) name:kReachabilityChangedNotification object:nil];
+    reachable =[Reachability reachabilityWithHostName:@"www.parse.com"];
+    [reachable startNotifier];
+    
     return YES;
 }
 
@@ -28,6 +34,7 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -36,10 +43,66 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void) reachabilityChanged:(NSNotification *)note
+{
+    NetworkStatus status = reachable.currentReachabilityStatus;
+    [self displayOrHideStatus:true];
+    BOOL connectionRequired = [reachable connectionRequired];
+    switch (status)
+    {
+        case NotReachable:        {
+            NSLog(@"N/A");
+            connectionRequired = NO;
+            break;
+        }
+        case ReachableViaWWAN:        {
+            NSLog(@"Reachable WWAN");
+            break;
+        }
+        case ReachableViaWiFi:        {
+            break;
+        }
+    }
+    if (connectionRequired)
+    {
+        NSLog(@"may require vpn");
+    }
+    
+}
+-(void) displayOrHideStatus:(BOOL) display
+{
+    if (display) {
+        if (noNetView == nil) {
+            noNetView = [[UIView alloc]  initWithFrame:CGRectMake(0, self.window.frame.size.height, self.window.frame.size.width, 40)];
+            UILabel *noStausLbl = [[UILabel alloc]  initWithFrame:CGRectMake(10, 0, noNetView.frame.size.width, noNetView.frame.size.height)];
+            [noStausLbl setTextColor:[UIColor whiteColor]];
+            [noStausLbl setText:@"No Connection."];
+            [noStausLbl setFont:[UIFont fontWithName:@"Avenir Light" size:14]];
+            [noStausLbl setTag:1];
+            [noNetView addSubview:noStausLbl];
+            [noNetView setBackgroundColor:[UIColor colorWithWhite:0.492 alpha:1.000]];
+        }
+        [self.window addSubview:noNetView];
+        [UIView animateWithDuration:2 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+            [noNetView setFrame:CGRectMake(0, self.window.frame.size.height-40, self.window.frame.size.width, 40)];
+        } completion:^(BOOL finished) {
+            [self performSelector:@selector(displayOrHideStatus:) withObject:false afterDelay:2];
+        }];
+        
+    }else{
+        [UIView animateWithDuration:1 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+            [noNetView setFrame:CGRectMake(0, self.window.frame.size.height, self.window.frame.size.width, 40)];
+        } completion:^(BOOL finished) {
+            [noNetView removeFromSuperview];
+        }];
+    }
+}
 @end
+
